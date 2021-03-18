8ead16e777758426383d5ce9b9e7ba4c2a390115
Sirisha Pylla <sirisha@likemindsconsulting.com>
2021-03-18 20:29:41 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/Sirisha-training/s3-bucket/aws"
   version = "1.21.0"
   # insert required variables here
-  bucket_prefix=Sirisha-Pylla
+  bucket_prefix="Sirisha-Pylla"
 }
